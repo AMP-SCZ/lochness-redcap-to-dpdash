a5e7008eeeadaa0f3ced3d99b16a682a009bec01
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-04-19 13:03:28 -0400

    closes issue #2

--- lib/parse_dict.py
+++ lib/parse_dict.py
@@ -1,11 +1,11 @@
 import csv
-
+import pandas as pd
 
 def parse(data_dictionary):
     assessments = {}
-    with open(data_dictionary) as redcap_dictionary:
-        redcap_reader = csv.DictReader(redcap_dictionary)
-        for dict_row in redcap_reader:
+    with open(data_dictionary, encoding='utf-8') as redcap_dictionary:
+        redcap_reader = pd.read_csv(redcap_dictionary)
+        for _, dict_row in redcap_reader.iterrows():
             assessment = dict_row['Form Name']
             if assessment != 'informed_consent':
                 variable_name = dict_row['Variable / Field Name']
